87758212c9fe6771eb3811523e70ab8664b6769c
winnie <winnie131212592@gmail.com>
2019-02-05 06:38:29 +0800

    refactor and separate views into templates

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -5,9 +5,3 @@
 
 		<script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 		<script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
-
-		
-		<?php $this->load->view('datatables/employees') ?>
-	
-	</body>
-</html>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,14 +1,13 @@
 <?php $this->load->view('templates/header') ?>
 
+
 		<h1 class="text-center">Sample CRUD</h1>
 
 		<br />
 
 
-
 	<div class="container">
 		<div class="row">	
-
 			<a class="btn btn-primary" data-toggle="modal" href='#modal-id'>
 			<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
 			Add</a>
@@ -31,63 +30,12 @@
 		</div>
 	</div>
 
+		
+	<?php $this->load->view('employee/form-add') ?>
 
 
-	<div class="modal fade" id="modal-id">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					<h4 class="modal-title">Add</h4>
-				</div>
-				<div class="modal-body">
-					
-					<form class="form-horizontal" action="">
-					
-					  <div class="form-group">
-					    <label class="control-label col-sm-4" for="fname">First name:</label>
-					    <div class="col-sm-8">
-					      <input type="text" class="form-control" id="fname" placeholder="Enter first name">
-					    </div>
-					  </div>
-
-					  <div class="form-group">
-					    <label class="control-label col-sm-4" for="lname">Last name:</label>
-					    <div class="col-sm-8">
-					      <input type="text" class="form-control" id="lname" placeholder="Enter last name">
-					    </div>
-					  </div>
-
-					  <div class="form-group">
-					    <label class="control-label col-sm-4" for="age">Age:</label>
-					    <div class="col-sm-8">
-					      <input type="number" min="0" class="form-control" id="age" placeholder="Enter age">
-					    </div>
-					  </div>
-
-
-					  <div class="form-group">
-					    <label class="control-label col-sm-4" for="position">Position:</label>
-					    <div class="col-sm-8">
-					    	<select class="form-control" id="position">
-							    <option>Customer Support</option>
-							    <option>Customer Support</option>
-							  </select>
-					    </div>
-					  </div>
-
-
-					</form><!-- end form -->
-
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-primary">Save</button>
-				</div>
-			</div>
-		</div>
-	</div>
-
-
-
-<?php $this->load->view('templates/footer') ?>
+	<?php $this->load->view('templates/footer') ?>
+	<?php $this->load->view('employee/datatable') ?>
+	
+	</body>
+</html>
