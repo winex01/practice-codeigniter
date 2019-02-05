147dd6329afafaa0e71dec64222ced2f6a29c481
winnie <winnie131212592@gmail.com>
2019-02-05 18:11:48 +0800

    add dump and dd custom helper

--- application/helpers/custom_helper.php
+++ application/helpers/custom_helper.php
@@ -51,3 +51,27 @@ if ( ! function_exists('confirm_delete'))
 }
 
 
+if ( ! function_exists('dd'))
+{
+    function dd($dump)
+    {
+        echo '<pre>';
+          var_dump($dump);
+        echo '</pre>';
+
+        die;
+              
+    }   
+}
+
+
+if ( ! function_exists('dump'))
+{
+    function dump($dump)
+    {
+        echo '<pre>';
+          var_dump($dump);
+        echo '</pre>';
+              
+    }   
+}
